872d5988c9791d466b7e5ffbe243e7df7269d1d3
Rita <mail@ritabuil.com>
2023-10-14 21:01:36 +0200

    Starting project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="font-bold">
+        <h1>Amazing Weather App!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
